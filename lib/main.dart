cecaa4f9cf9bb3541f8903507e79a855a0ff4f52
Janto Andriano <38934947+JantoAndriano@users.noreply.github.com>
2019-05-09 17:06:47 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flightapp/HomeScreen.dart';
+
+
+void main()=>runApp(MaterialApp(
+  title: 'FlightList',
+  debugShowCheckedModeBanner: false,
+  home: HomeScreen(),
+  theme: appTheme,
+  )
+);
+
+
+
+
